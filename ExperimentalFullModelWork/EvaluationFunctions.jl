8910c3832e64619be70595fe9ccac1c9ed820c89
egreenberg7 <egreen70@jhu.edu>
2023-07-06 10:43:06 -0400

    Refactor experimental oscillator code

---
+++ ExperimentalFullModelWork/EvaluationFunctions.jl
@@ -0,0 +1,85 @@
+using DifferentialEquations
+using Statistics
+using Peaks
+using FFTW
+
+"""    
+Checks if from time:end of the solution the standard deviation is greater than 0.01 * the mean for the data points.
+Default time is 80 seconds. Makes it so fft unneeded on clearly nonoscillatory solutions.
+"""
+function isSteady(Y::ODESolution; time::Float64 = 80.0)
+    timeIndex = findfirst(x -> x > time, Y.t) #Find index of time greater than 80
+    solutionInterval = Y.u[timeIndex:end]
+    meanVal = mean(solutionInterval)
+    stdVal = std(solutionInterval)
+    return stdVal > 0.01 * meanVal
+end
+
+function getSTD(fft_peakindxs::Vector{Int}, fft_arrayData::Vector{Float64}; window::Int = 1)#, window_ratio::Float64) #get average standard deviation of fft peak indexes
+    arrLen = length(fft_arrayData)
+    sum_std = @inbounds sum(std(fft_arrayData[max(1, ind - window):min(arrLen, ind + window)]) for ind in fft_peakindxs) #* sum rolling window of standard deviations
+    return sum_std / length(fft_peakindxs) #* divide by number of peaks to get average std
+end
+
+"""Return normalized FFT of solution vector"""
+function getFrequencies(timeseries::Vector{Float64}) #todo fix normalization or something 
+    res = abs.(rfft(timeseries))
+    return res ./ cld(length(timeseries), 2) #* normalize amplitudes
+end
+
+"""Cost function to be plugged into eval_fitness wrapper"""
+function CostFunction(sol::ODESolution)::Vector{Float64}
+    #*get| the fft of the solution
+    if isSteady(sol)
+        return [1.0, 0.0, 0.0]
+    end
+    fftData = getFrequencies(sol.u)
+    fft_peakindexes, fft_peakvals = findmaxima(fftData,10) #* get the indexes of the peaks in the fft
+    time_peakindexes, time_peakvals = findmaxima(sol.u,5) #* get the times of the peaks in the fft
+    if length(fft_peakindexes) < 2 || length(time_peakindexes) < 2 #* if there are no peaks in either domain, return 0
+        return [1.0, 0.0, 0.0]
+    end
+    std = getSTD(fft_peakindexes, fftData) #* get the average standard deviation of the peaks in frequency domain
+
+    #* Compute the period and amplitude
+    period, amplitude = getPerAmp(sol, time_peakindexes, time_peakvals)
+
+    #* Return cost, period, and amplitude as a vector
+    return [-std, period, amplitude]
+end
+
+function eval_fitness_catcherrors(sol::ODESolution)
+    Y = nothing
+    try 
+        if sol.retcode in (ReturnCode.Unstable, ReturnCode.MaxIters) || any(x==1 for array in isnan.(sol) for x in array) || any(x==1 for array in isless.(sol, 0.0) for x in array)
+            return [1.0, 0.0, 0.0]
+        end
+    catch e 
+        if isa(e, DomainError) #catch domain errors
+            return [1.0,0.0,0.0]
+        else
+            rethrow(e) #rethrow other errors
+        end
+    end
+    if !(sol.retcode in (ReturnCode.Success, ReturnCode.Default)) #I added this
+        println("FAILURE")
+        return [1.0,0.0,0.0]
+    end
+    return CostFunction(sol)
+end
+
+function getPerAmp(sol::ODESolution, indx_max::Vector{Int}, vals_max::Vector{Float64})
+    #* Find peaks of the minima too 
+    indx_min, vals_min = findminima(sol[1,:], 1)
+
+    if length(indx_max) < 1 || length(indx_min) < 1 #todo need to fix this, either keep or move check into cost function
+        return 0.0, 0.0
+    end
+
+    #* Calculate amplitudes and periods
+    @inbounds pers = (sol.t[indx_max[i+1]] - sol.t[indx_max[i]] for i in 1:(length(indx_max)-1))
+    @inbounds amps = ((vals_max[i] - vals_min[i])/2 for i in 1:min(length(indx_max), length(indx_min)))
+    # @inbounds amps = 0.5 .* (vals_max .- vals_min)[1:min(length(indx_max), length(indx_min))]
+
+    return mean(pers), mean(amps)
+end
---
+++ ExperimentalFullModelWork/ExperimentalOscillators.jl
@@ -0,0 +1,66 @@
+using DifferentialEquations
+using Plots
+using Catalyst
+using DataFrames
+using CSV
+using Statistics
+using Peaks
+using FFTW
+include("EvaluationFunctions.jl")
+include("Constants.jl")
+
+oscData = DataFrame(DF=Float64[], ka7=Float64[], ka4=Float64[], ka1=Float64[], kb1=Float64[],
+                    fit=Float64[],per=Float64[], amp=Float64[],L=Float64[], K=Float64[], P=Float64[], A=Float64[])
+nonoscData = DataFrame(DF=Float64[], ka7=Float64[], ka4=Float64[], ka1=Float64[], kb1=Float64[])
+
+
+for df in dfRange
+    dfest = df
+    for ka7 in kaRange
+        ka7est = ka7
+        for ka4 in kaRange
+            ka4est = ka4
+            for ka1 in kaRange
+                ka1est = ka1
+                for kb1 in kbRange
+                    kb1est = kb1
+                    #Set up parameter values
+                    psym = [:ka1 => ka1est, :kb1 => kb1est, :kcat1 => Km1exp * ka1est - kb1est, :ka2 => ka2exp, :kb2 => kb2exp,
+                            :ka3 => ka3exp, :kb3 => kb3exp, :ka4 => ka4est, :kb4 => Kd4exp * ka4est, :ka7 => ka7est, 
+                            :kb7 => Km7exp * ka7est - kcat7exp, :kcat7exp => 85.3, :y => dfest]
+                    p = [x[2] for x in psym]
+                    #Count how many oscillatory solutions
+                    oscFound = 0
+                    for i in 1:numU0combos
+                        curU0 = u0combos[i,:]
+                        u0[1:4] = curU0[1:4]
+                        currentSol = solve(remake(prob, u0=u0, p=p), Rosenbrock23(), saveat=0.1, save_idxs=1, maxiters=10000, verbose=false)
+                        costPerAmp = eval_fitness_catcherrors(currentSol)
+                        if(costPerAmp[1] < 0)
+                            oscFound+=1
+                            if oscFound >= 3
+                                push!(oscData, Dict(:DF => df, :ka7 => ka7, :ka4 => ka4, :ka1 => ka1, :kb1 => kb1,
+                                                    :fit => costPerAmp[1], :per => costPerAmp[2], :amp => costPerAmp[3],
+                                                    :L => curU0[1],:K => curU0[2],:P => curU0[3],:A => curU0[4]))
+                                break
+                            end
+                        end
+                    end
+                    if oscFound < 3
+                        push!(nonoscData, Dict(:DF => df, :ka7 => ka7, :ka4 => ka4, :ka1 => ka1, :kb1 => kb1))
+                    end
+                    oscFound = 0
+                end
+            end
+        end
+        println("You are at ka7=$(ka7) and df=$(df)")
+    end
+    oscFileName = "OscCsvs/Osc_df_$(df).csv"
+    nonoscFileName = "OscCsvs/No_osc_df$(df).csv"
+    CSV.write(oscFileName,oscData)
+    CSV.write(nonoscFileName,nonoscData)
+end
+
+
+
+                            
