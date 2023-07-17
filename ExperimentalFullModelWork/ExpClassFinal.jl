2acbc907db64d3c3a0cc075c39ee32ad767747b3
egreenberg7 <egreen70@jhu.edu>
2023-07-17 17:53:55 -0400

    Add progress messages, debugging

--- ExperimentalFullModelWork/ExpClassFinal.jl
+++ ExperimentalFullModelWork/ExpClassFinal.jl
@@ -14,7 +14,7 @@ Make directory where output CSVs where be stored. Also makes README
 file detialing ranges used on the program run and tests that CSVs can successfully
 be created.
 """
-function setOutputDir(df=dfRange[1],ka7=ka7minExponent)
+function setOutputDir(df=dfRange[1], ka7=ka7minExponent)
     mkdir("$(df)DF_$(ka7)ka7min")
     cd("$(df)DF_$(ka7)ka7min")
     mkdir("AllData")
@@ -25,53 +25,55 @@ function setOutputDir(df=dfRange[1],ka7=ka7minExponent)
             println(io, "$(i[1]): ($(i[2][1]),$(i[2][end])) with $(length(i[2])) elements")
         end
     end
-    testdf = DataFrame(Dict("X"=>0.0))
-    CSV.write("AllData/test.csv", testdf)      
+    testdf = DataFrame(Dict("X" => 0.0))
+    CSV.write("AllData/test.csv", testdf)
+    progressFile = open("progress.txt", "w")
+    return progressFile
 end
 
 """
 Loop over all ranges and evaluate if oscillatory.
 """
-function makeSolutionCSVs(dfRange=dfRange, ka7minExponent=ka7minExponent, ka7maxExponent=ka7maxExponent, kaRange=kaRange, kbRange=kbRange, Lrange=Lrange, Prange=Prange,Krange=Krange,Arange=Arange)
-    setOutputDir()
-    numRateCombos = length(dfRange) * (ka7maxExponent-ka7minExponent + 1) * length(kaRange) * length(kaRange) * length(kbRange)
+function makeSolutionCSVs(dfRange=dfRange, ka7minExponent=ka7minExponent, ka7maxExponent=ka7maxExponent, kaRange=kaRange, kbRange=kbRange, Lrange=Lrange, Prange=Prange, Krange=Krange, Arange=Arange)
+    progressFile = setOutputDir()
+    numRateCombos = length(dfRange) * (ka7maxExponent - ka7minExponent + 1) * length(kaRange) * length(kaRange) * length(kbRange)
 
     for dfest in dfRange
         #Initialize dictionary to store data in, will be converted to dataframe at termination
-        data = Dict(:df=>zeros(Float64,numRateCombos), :ka7=>zeros(Float64, numRateCombos), 
-                    :ka4=>zeros(Float64,numRateCombos), :ka1=>zeros(Float64,numRateCombos),:kb1=>zeros(Float64,numRateCombos),
-                    :fit=>zeros(Float64,numRateCombos), :per=>zeros(Float64,numRateCombos), :amp=>zeros(Float64,numRateCombos), 
-                    :L=>zeros(Float64,numRateCombos), :K=>zeros(Float64,numRateCombos), :P=>zeros(Float64,numRateCombos), :A=>zeros(Float64,numRateCombos), 
-                    :oscFound=>zeros(Int64,numRateCombos))
-        println("#################")
-        println("df = $(df)")
-        println("#################")
-        flush(stdout)
+        data = Dict(:df => zeros(Float64, numRateCombos), :ka7 => zeros(Float64, numRateCombos),
+            :ka4 => zeros(Float64, numRateCombos), :ka1 => zeros(Float64, numRateCombos), :kb1 => zeros(Float64, numRateCombos),
+            :fit => zeros(Float64, numRateCombos), :per => zeros(Float64, numRateCombos), :amp => zeros(Float64, numRateCombos),
+            :L => zeros(Float64, numRateCombos), :K => zeros(Float64, numRateCombos), :P => zeros(Float64, numRateCombos), :A => zeros(Float64, numRateCombos),
+            :oscFound => zeros(Int64, numRateCombos))
+        println(progressFile, "#################")
+        println(progressFile, "df = $(dfest)")
+        println(progressFile, "#################")
+        flush(progressFile)
         #Initialize new dataframe at each df value
         count = 0
         numericalErrorCount = 0
         for ka7est in kaRange[ka7minExponent:ka7maxExponent]
-            println("You are at ka7=$(ka7est).")
-            flush(stdout)
+            println(progressFile, "You are at ka7=$(ka7est).")
+            flush(progressFile)
             for ka4est in kaRange
-                println("ka4 = $(ka4est)")
-                flush(stdout)
+                println(progressFile, "ka4 = $(ka4est)")
+                flush(progressFile)
                 for ka1est in kaRange
                     for kb1est in kbRange
                         #Set up parameter values
                         psym = [:ka1 => ka1est, :kb1 => kb1est, :kcat1 => Km1exp * ka1est - kb1est, :ka2 => ka2exp, :kb2 => kb2exp,
-                                :ka3 => ka3exp, :kb3 => kb3exp, :ka4 => ka4est, :kb4 => Kd4exp * ka4est, :ka7 => ka7est, 
-                                :kb7 => Km7exp * ka7est - kcat7exp, :kcat7exp => 85.3, :y => dfest]
+                            :ka3 => ka3exp, :kb3 => kb3exp, :ka4 => ka4est, :kb4 => Kd4exp * ka4est, :ka7 => ka7est,
+                            :kb7 => Km7exp * ka7est - kcat7exp, :kcat7exp => 85.3, :y => dfest]
                         p = [x[2] for x in psym]
                         #Count how many oscillatory solutions
                         oscFound = 0
                         for i in 1:numU0combos
-                            curU0 = u0combos[i,:]
+                            curU0 = u0combos[i, :]
                             u0[1:4] = curU0[1:4]
-                            costPerAmp = adaptiveSolve(prob,u0,shortSpan,longSpan,p; abstol=1e-8,reltol=1e-6)
+                            costPerAmp = adaptiveSolve(prob, u0, shortSpan, longSpan, p; abstol=1e-8, reltol=1e-6)
                             retcode = costPerAmp[1]
                             if retcode < 0.0
-                                oscFound+=1
+                                oscFound += 1
                                 if oscFound >= 3
                                     data[:df][count] = dfest
                                     data[:ka7][count] = ka7est
@@ -111,10 +113,9 @@ function makeSolutionCSVs(dfRange=dfRange, ka7minExponent=ka7minExponent, ka7max
             allDataName = "AllData/df_($dfest)_ka7_$(ka7est).csv"
             CSV.write(allDataName, datadf)
         end
-        println("At df = $(dfest), $(numericalErrorCount) numerical errors occurred.")
-        flush(stdout)
+        println(progressFile, "At df = $(dfest), $(numericalErrorCount) numerical errors occurred.")
+        flush(progressFile)
     end
 end
 
 makeSolutionCSVs()
-                            
