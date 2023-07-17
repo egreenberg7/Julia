ef87b6f1c960fd260306b49be3463e40d2b4f504
egreenberg7 <egreen70@jhu.edu>
2023-07-16 21:02:55 -0400

    Change constants and output directories for runs on external computers

---
+++ ExperimentalFullModelWork/sshConstants.jl
@@ -0,0 +1,113 @@
+using Catalyst
+
+#Known parameters
+const Km1exp = 15.0
+const ka2exp = 0.7*10^-3
+const kb2exp = 2.0 * 10^-3
+const ka3exp = 0.118
+const kb3exp = 0.0609
+const Kd4exp =29.0
+const Km7exp = 39.0
+const kcat7exp = 85.3
+
+#Estimates for unknown parameters
+ka1est = 0.9 * 10^-3 #Slightly less than ka2
+kb1est = 0.2 * 10^-3
+ka4est = 0.12 #Estimating LpA bindking to K (ka3) similar to LpA binding to L
+ka7est = 0.9 #Pretty much guess, around ka1
+dfest = 750
+
+#parameter list Changed this around
+"""ka1, kb1, kcat1, ka2, kb2, ka3, kb3, ka4, kb4, ka7, kb7, kcat7, y"""
+psym = [:ka1 => ka1est, :kb1 => kb1est, :kcat1 => Km1exp * ka1est - kb1est, :ka2 => ka2exp, :kb2 => kb2exp,
+        :ka3 => ka3exp, :kb3 => kb3exp, :ka4 => ka4est, :kb4 => Kd4exp * ka4est, :ka7 => ka7est, 
+        :kb7 => Km7exp * ka7est - kcat7exp, :kcat7exp => 85.3, :y => dfest]
+p = [x[2] for x in psym]
+
+    
+#initial condition list
+usym = [:L => 5, :K => 0.1, :P => 0.1, :A => 0.1, :Lp => 0., :LpA => 0.0, :LK => 0., 
+        :LpP => 0., :LpAK => 0., :LpAP => 0., :LpAKL => 0., :LpAPLp => 0., :AK => 0., :AP => 0., 
+        :AKL => 0., :APLp => 0.]
+u0 = [x[2] for x in usym]
+
+#Reaction network
+const fullrn = @reaction_network fullrn begin
+    @parameters ka1 kb1 kcat1 ka2 kb2 ka3 kb3 ka4 kb4 ka7 kb7 kcat7 y
+    @species L(t) K(t) P(t) A(t) Lp(t) LpA(t) LK(t) LpP(t) LpAK(t) LpAP(t) LpAKL(t) LpAPLp(t) AK(t) AP(t) AKL(t) APLp(t)
+    # ALIASES: L = PIP, Lp = PIP2, K = Kinase (PIP5K), P = Phosphatase (Synaptojanin), A = AP2 
+    # reactions between the same binding interfaces will have the same rate constant no matter the dimensionality or complex
+    (ka1,kb1), L + K <--> LK # L binding to kinase
+    kcat1, LK --> Lp + K # L phosphorylation by kinase into Lp
+    (ka2,kb2), Lp + A <--> LpA # Lp binding to AP2 adaptor
+    (ka3,kb3), LpA + K <--> LpAK # Membrane-bound adaptor binding to kinase
+    (ka1*y,kb1), LpAK + L <--> LpAKL # 2D reaction: Membrane-bound kinase binds to L with greater affinity as determined by y (V/A)
+    kcat1, LpAKL --> Lp + LpAK # L phosphorylation by kinase into Lp, same as 3D: first order reactions aren't dependent on dimensionality 
+    (ka7,kb7), Lp + P <--> LpP # Lp binding to phosphatase
+    kcat7, LpP --> L + P # L dephosphorylation by phosphatase
+    (ka4,kb4), LpA + P <--> LpAP # Membrane-bound adaptor binding to phosphatase 
+    (ka7*y,kb7), Lp + LpAP <--> LpAPLp # 2D reaction: Membrane-bound phosphatase binds to Lp with greater affinity as determined by y (V/A)
+    kcat7, LpAPLp --> L + LpAP # L dephosphorylation by phosphatase, same as 3D: first order reactions aren't dependent on dimensionality
+
+    #previously excluded reactions, all possible combinations possible in vitro
+    (ka2,kb2), Lp + AK <--> LpAK
+    (ka2*y,kb2), Lp + AKL <--> LpAKL
+    (ka2,kb2), Lp + AP <--> LpAP
+    (ka2*y,kb2), Lp + APLp <--> LpAPLp
+    (ka3,kb3), A + K <--> AK
+    (ka4,kb4), A + P <--> AP
+    (ka3,kb3), A + LK <--> AKL
+    (ka4,kb4), A + LpP <--> APLp
+    (ka3*y,kb3), LpA + LK <--> LpAKL
+    (ka4*y,kb4), LpA + LpP <--> LpAPLp
+    (ka1,kb1), AK + L <--> AKL #binding of kinase to lipid
+    kcat1, AKL --> Lp + AK #phosphorylation of lipid
+    (ka7,kb7), AP + Lp <--> APLp #binding of phosphatase to lipid
+    kcat7, APLp --> L + AP #dephosphorylation of lipid
+end
+
+#Time bounds for ODE problem #// TODO make adaptive integration range
+const shortSpan = 100.0
+const longSpan = 600.0
+
+#Set up ODE problem
+const prob = ODEProblem(fullrn, u0, (0.0, shortSpan), p)
+
+
+#RANGES THAT VARY BY POWER OF 10
+#// TODO DIMENSIONALITY CAN GO TO 10,000
+dfRange = 10.0 .^ ((-2:8) ./2) #exponential range from 0.1 to 1000
+const kaRange = 10.0 .^ ((-6:2) ./2)
+const kbRange = 10.0 .^ ((-6:6) ./2)
+
+#// TODO Double resolution
+const Lrange = 10.0 .^ (-4:4) ./2
+const Krange = 10.0 .^ (-6:4) ./2
+const Prange = 10.0 .^ (-6:2) ./2
+const Arange = 10.0 .^ (-4:4) ./2
+
+#Set up array of initial conditions that will be tested in advance for ease of later code
+function makeCombos(Lrange, Krange, Prange, Arange)
+    arr = Array{Float64}(undef, length(Lrange)*length(Krange)*length(Arange)*length(Prange),4)
+    count = 1
+    for L in Lrange
+        for K in Krange
+            for P in Prange
+                for A in Arange
+                    arr[count, 1] = L
+                    arr[count, 2] = K
+                    arr[count, 3] = P
+                    arr[count, 4] = A
+                    count += 1
+                end
+            end
+        end
+    end
+    return arr
+end
+
+const u0combos = makeCombos(Lrange, Krange, Prange, Arange)
+const numU0combos = length(u0combos[:,1])
+
+#// TODO Set to const once ranges finalized
+
---
+++ ExperimentalFullModelWork/sshExperimentalOscillators.jl
@@ -0,0 +1,97 @@
+using DataFrames
+using CSV
+include("EvaluationFunctions.jl")
+include("sshConstants.jl")
+
+dfRange = [parse(Float64,i) for i in ARGS]
+
+"""
+Make directory where output CSVs where be stored. Also makes README 
+file detialing ranges used on the program run and tests that CSVs can successfully
+be created.
+"""
+function setOutputDir()
+    mkdir("osc_dfs")
+    cd("osc_dfs")
+
+    ranges = [dfRange, kaRange, kbRange, Lrange, Krange, Prange, Arange]
+    rangenames = ["dfRange", "kaRange", "kbRange", "Lrange", "Krange", "Prange", "Arange"]
+    open("README", "a") do io
+        for i in zip(rangenames, ranges)
+            println(io, "$(i[1]): ($(i[2][1]),$(i[2][end])) with $(length(i[2])) elements")
+        end
+    end
+    testdf = DataFrame(Dict("X"=>0.0))
+    CSV.write("test.csv", testdf)      
+end
+
+function makeSolutionCSVs()
+    setOutputDir()
+    for df in dfRange
+        println("#################")
+        println("df = $(df)")
+        println("#################")
+        flush(stdout)
+        #Initialize new dataframe at each df value
+        oscData = DataFrame(DF=Float64[], ka7=Float64[], ka4=Float64[], ka1=Float64[], kb1=Float64[],
+                    fit=Float64[],per=Float64[], amp=Float64[],L=Float64[], K=Float64[], P=Float64[], A=Float64[])
+        nonoscData = DataFrame(DF=Float64[], ka7=Float64[], ka4=Float64[], ka1=Float64[], kb1=Float64[])
+        dfest = df
+        numericalErrorCount = 0
+        for ka7 in kaRange
+            ka7est = ka7
+            println("You are at ka7=$(ka7) and df=$(df)")
+            flush(stdout)
+            for ka4 in kaRange
+                ka4est = ka4
+                println("ka4 = $(ka4)")
+                flush(stdout)
+                for ka1 in kaRange
+                    ka1est = ka1
+                    for kb1 in kbRange
+                        kb1est = kb1
+                        #Set up parameter values
+                        psym = [:ka1 => ka1est, :kb1 => kb1est, :kcat1 => Km1exp * ka1est - kb1est, :ka2 => ka2exp, :kb2 => kb2exp,
+                                :ka3 => ka3exp, :kb3 => kb3exp, :ka4 => ka4est, :kb4 => Kd4exp * ka4est, :ka7 => ka7est, 
+                                :kb7 => Km7exp * ka7est - kcat7exp, :kcat7exp => 85.3, :y => dfest]
+                        p = [x[2] for x in psym]
+                        #Count how many oscillatory solutions
+                        oscFound = 0
+                        for i in 1:numU0combos
+                            curU0 = u0combos[i,:]
+                            u0[1:4] = curU0[1:4]
+                            costPerAmp = adaptiveSolve(prob,u0,shortSpan,longSpan,p; abstol=1e-8,reltol=1e-6)
+                            retcode = costPerAmp[1]
+                            if retcode < 0.0
+                                oscFound+=1
+                                if oscFound >= 3
+                                    push!(oscData, Dict(:DF => df, :ka7 => ka7, :ka4 => ka4, :ka1 => ka1, :kb1 => kb1,
+                                                        :fit => costPerAmp[1], :per => costPerAmp[2], :amp => costPerAmp[3],
+                                                        :L => curU0[1],:K => curU0[2],:P => curU0[3],:A => curU0[4]))
+                                    break
+                                end
+                            elseif retcode in [1.0, 1.5]
+                                numericalErrorCount += 1
+                            end
+                        end
+                        if oscFound < 3
+                            push!(nonoscData, Dict(:DF => df, :ka7 => ka7, :ka4 => ka4, :ka1 => ka1, :kb1 => kb1))
+                        end
+                        oscFound = 0
+                    end
+                end
+            end
+        end
+        println("At df = $(df), $(numericalErrorCount) numerical errors occurred.")
+        flush(stdout)
+        oscFileName = "Osc_df_$(df).csv"
+        nonoscFileName = "No_osc_df_$(df).csv"
+        CSV.write(oscFileName,oscData)
+        CSV.write(nonoscFileName,nonoscData)
+    end
+end
+
+makeSolutionCSVs()
+
+
+                            
